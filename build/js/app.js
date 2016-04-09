98b7aaa5fd20467825f41544df748da2e1d5bf74
Melanie Burger <melanie.brgr@gmail.com>
2016-04-08 20:17:24 -0400

    test2

--- build/js/app.js
+++ build/js/app.js
@@ -65,14 +65,14 @@ console.log('1');
 if(document.readyState == 'complete')
 {
 	console.log('2');
-  var app = new flappyBird.FlappyBird();
+  var app = new FlappyBird.flappyBird();
   app.run();
 }
 else
 {
 console.log('3');
   document.addEventListener('DOMContentLoaded', function() {
-    var app = new flappyBird.FlappyBird();
+    var app = new FlappyBird.flappyBird();
     app.run();
   });
 }
